dfdea00696db5b7fd303f8232a0c032c0e5505db
Omar Khachidze <omar.khachidze.1@btu.edu.ge>
2022-02-17 23:19:08 +0400

    add splash screen for both, dark and light mode

--- app/src/main/java/ge/example/githubapidemo/MainActivity.kt
+++ app/src/main/java/ge/example/githubapidemo/MainActivity.kt
@@ -1,11 +1,90 @@
 package ge.example.githubapidemo
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        customizeSplashScreen(splashScreen)
+
     }
+
+    private fun customizeSplashScreen(splashScreen: SplashScreen) {
+        customizeSplashScreenExit(splashScreen)
+    }
+
+    private fun customizeSplashScreenExit(splashScreen: SplashScreen) {
+        splashScreen.setOnExitAnimationListener { splashScreenViewProvider ->
+
+            showSplashExitAnimator(splashScreenViewProvider.view) {
+                splashScreenViewProvider.remove()
+            }
+
+            showSplashIconExitAnimator(splashScreenViewProvider.iconView) {
+                splashScreenViewProvider.remove()
+            }
+        }
+    }
+
+    private fun showSplashExitAnimator(splashScreenView: View, onExit: () -> Unit = {}) {
+
+        val scaleOut = ObjectAnimator.ofFloat(
+            splashScreenView,
+            View.SCALE_X,
+            View.SCALE_Y,
+            Path().apply {
+                moveTo(1.0f, 1.0f)
+                lineTo(0f, 0f)
+            }
+        )
+        AnimatorSet().run {
+            duration = resources.getInteger(R.integer.splash_exit_total_duration).toLong()
+            interpolator = AnticipateInterpolator()
+            playTogether(scaleOut)
+
+            doOnEnd {
+                onExit()
+            }
+            start()
+        }
+    }
+
+
+    private fun showSplashIconExitAnimator(iconView: View, onExit: () -> Unit = {}) {
+
+        val scaleOut = ObjectAnimator.ofFloat(
+            iconView,
+            View.SCALE_X,
+            View.SCALE_Y,
+            Path().apply {
+                moveTo(1.0f, 1.0f)
+                lineTo(0.3f, 0.3f)
+            }
+        )
+
+        AnimatorSet().run {
+            interpolator = AnticipateInterpolator()
+            duration =
+                resources.getInteger(R.integer.splash_exit_total_duration)
+                    .toLong()
+
+            playTogether(scaleOut)
+            doOnEnd {
+                onExit()
+            }
+            start()
+        }
+    }
+
 }
