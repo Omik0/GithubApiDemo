6a3e0399540cc33c71de7b915bfd2c771f50fd7f
Omar Khachidze <omar.khachidze.1@btu.edu.ge>
2022-02-19 15:16:06 +0400

    change lib file to libs and add in to gitignore file

--- app/src/main/cpp/libs.cpp
+++ app/src/main/cpp/libs.cpp
@@ -1,8 +1,9 @@
 #include <jni.h>
 #include <string>
+
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_ge_example_githubapidemo_Keys_githubToken(JNIEnv *env, jobject thiz) {
-    std::string github_token = "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";
+    std::string github_token = "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";
     return env->NewStringUTF(github_token.c_str());
 }
