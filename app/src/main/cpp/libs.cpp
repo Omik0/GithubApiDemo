aa80ebd1ae4714c5d9e9a184babece19997af59c
Omar Khachidze <omar.khachidze.1@btu.edu.ge>
2022-02-19 15:24:35 +0400

    lastly switch libs file to lib file

--- app/src/main/cpp/libs.cpp
+++
@@ -1,9 +0,0 @@
-#include <jni.h>
-#include <string>
-
-extern "C"
-JNIEXPORT jstring JNICALL
-Java_ge_example_githubapidemo_Keys_githubToken(JNIEnv *env, jobject thiz) {
-    std::string github_token = "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";
-    return env->NewStringUTF(github_token.c_str());
-}
